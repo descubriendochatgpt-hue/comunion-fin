d5280766ab7de6b043fc4f0fa8a761bbca093b9a
descubriendochatgpt-hue <descubriendochatgpt@gmail.com>
2026-08-31 13:16:47 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* Paste your Apps Script web app URL here after deploying.
-   It looks like: https://script.google.com/macros/s/AKfy..../exec  */
-window.FSL_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE"
-};
